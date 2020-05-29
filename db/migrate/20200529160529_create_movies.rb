9c58b883541128f6ccfbc836ec59935a5b9a80db
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-05-29 17:10:07 +0100

    - migrated db - created controller with new, create, index, show, update, edit, destroy actions

--- db/migrate/20200529160529_create_movies.rb
+++ db/migrate/20200529160529_create_movies.rb
@@ -1,15 +1,14 @@
-class CreateJobs < ActiveRecord::Migration[6.0]
+class CreateMovies < ActiveRecord::Migration[6.0]
   def change
-    create_table :jobs do |t|
-      t.string :name
+    create_table :movies do |t|
+      t.text :name
       t.string :studio
       t.string :guidance
       t.string :director
       t.string :starring
-      t.number :runtime
-      t.number :ticket_price
+      t.string :runtime
+      t.string :ticket_price
       t.text :description
-      t.blob :image
 
       t.timestamps
     end
